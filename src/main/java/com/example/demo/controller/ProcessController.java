272fcc2484c0ca73c5f50b45c3955b64f58ac26f
Glenn <CoderGlenn@gmail.com>
2024-10-14 21:22:14 +0800

    Update ProcessController.java

--- src/main/java/com/example/demo/controller/ProcessController.java
+++ src/main/java/com/example/demo/controller/ProcessController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProcessController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ProcessController.class);
+
     @Autowired
     private Process process;
 
