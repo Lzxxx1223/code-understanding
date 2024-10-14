007ff8ff1e0b746e2910182681a4e0cd10c653e9
Glenn <CoderGlenn@gmail.com>
2024-10-14 21:22:51 +0800

    Update ProcessController.java

--- src/main/java/com/example/demo/controller/ProcessController.java
+++ src/main/java/com/example/demo/controller/ProcessController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Response;
 import com.example.demo.service.Process;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
